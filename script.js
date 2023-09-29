66f6c3f782cf307683316c5a06a3dedee0360e5b
Lionel jevon <lioneljevon1212@gmail.com>
2023-09-29 09:17:56 +0700

    video 2
    Fungsi

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-//video 1 penulisan script js outline / export
-console.log("Javascript");
+//Function atau Fungsi
+function Fungsi (){
+    console.log("Ini fungsi");
+}
+
+//Fungsi dengan nilai kembalian
+function FungsiReturn(){
+    return "aku belajar function";
+}
+//Fungsi dengan Parameter
+function Luas (sisi){
+    return sisi * sisi;
+}
+Fungsi();
+console.log(FungsiReturn())
+console.log(Luas(23));
 
