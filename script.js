8399af1c6405f166a788143114b7b4ee3cc18cef
Lionel jevon <lioneljevon1212@gmail.com>
2023-10-12 12:32:44 +0700

    Video 4
    Javascript selector

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-//video 3 dom html js
-document.querySelector("div").innerHTML = "lorem ipsum dolor sit amir";
-// menggunakan document.getelement method
-document.getElementById("tes").innerText = "Tes";
-
-function Fungsi(){
-    document.querySelector("#tes").innerText = "Berubah";
-} 
+//video 4 dom query selector
+klik.onclick = function (){
+    let header = document.querySelector("h1"); //mengambil dari elemen
+    let kelas  = document.querySelector(".container"); //mengambil dari identifier kelas
+    let aidi   = document.querySelector("#par");
+    
+    header.innerText = "Javascript";
+    kelas.style.backgroundColor = "red";
+    aidi.innerText = "Java";
+};
 
