c16af5355127fc6b18a71ca2dba32918dc83ef09
Lionel jevon <lioneljevon1212@gmail.com>
2023-10-13 09:46:28 +0700

    Video 5
    QuerySelectorAll

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-//video 4 dom query selector
-klik.onclick = function (){
-    let header = document.querySelector("h1"); //mengambil dari elemen
-    let kelas  = document.querySelector(".container"); //mengambil dari identifier kelas
-    let aidi   = document.querySelector("#par");
-    
-    header.innerText = "Javascript";
-    kelas.style.backgroundColor = "red";
-    aidi.innerText = "Java";
+//video 5 QuerySelectorall
+
+par.onclick = () => {
+    //memeriksa array img
+    console.log(document.querySelectorAll('img'));
+     //fungsi querySelctorAll adalah untuk menunjuk semua elemen img membentuk sebuah array 
+    //membuat random
+    min = Math.ceil(0);
+    max = Math.floor(2);
+
+    let n = Math.floor(Math.random()* (max - min +1));
+    console.log(n);
+    document.querySelectorAll('img')[n].style.visibility = 'visible';
 };
 
