88318b1251bf68df839b135e9754518805f7ee15
Lionel jevon <lioneljevon1212@gmail.com>
2023-10-26 19:27:17 +0700

    Video 7
    JS Event

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//video 6 DOM form 
+//video 7 JS Event
 
 var tampil = document.getElementById('tampil');
 
@@ -7,4 +7,11 @@ tampil.onclick = () => {
     // '.value' untukmenngambil input ddari html nya denganbenar 
     document.querySelector('#tempelNama').value = document.querySelector('#inputNama').value;
     document.querySelector('#para').textContent = document.querySelector('#inputNama').value;
-}
+};
+
+btn.onclick = tampil;
+btn.onclick = ()=> alert('JavaScript');
+btn.addEventListener('mouseover',tampil);
+btn.addEventListener('mouseover', ()=>{
+    alert('mouseover event');
+});
